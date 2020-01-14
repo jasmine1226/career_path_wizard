875ee0a7e4d0606cc340d10096e2128ef1b3e0ff
jasmine1226 <jasmine1226@gmail.com>
2020-01-13 23:16:57 -0800

    render course paths

--- frontend/src/components/careerPath.js
+++ frontend/src/components/careerPath.js
@@ -6,8 +6,31 @@ class CareerPaths {
   }
 
   fetchAndLoadCareerPath() {
-    const careerPaths = this.adapter.getCareerPaths().then(json => {
-      console.log(json);
+    const careerPaths = this.adapter
+      .getCareerPaths()
+      .then(json => {
+        return json.data.forEach(careerPath => {
+          this.careerPaths.push({
+            id: careerPath.id,
+            career_track: careerPath.attributes.career_track
+          });
+        });
+      })
+      .then(() => this.render());
+  }
+
+  render() {
+    const careerPathContainer = document.getElementById(
+      "career-path-container"
+    );
+    const list = document.createElement("ul");
+    list.classList.add("list-group");
+    this.careerPaths.forEach(careerPath => {
+      const item = document.createElement("li");
+      item.innerHTML = careerPath.career_track;
+      item.classList.add("list-group-item");
+      list.appendChild(item);
     });
+    careerPathContainer.appendChild(list);
   }
 }
