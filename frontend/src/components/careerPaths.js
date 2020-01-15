41aea824ca334592087a14868a5e8511d7193c64
jasmine1226 <jasmine1226@gmail.com>
2020-01-14 22:30:14 -0800

    simplified careerPaths render()

--- frontend/src/components/careerPaths.js
+++ frontend/src/components/careerPaths.js
@@ -12,18 +12,14 @@ class CareerPaths {
         json.data.forEach(careerPath =>
           this.careerPaths.push(new CareerPath(careerPath))
         );
-        console.log(this.careerPaths);
       })
       .then(() => this.render());
   }
 
   render() {
     const list = document.getElementById("career-path-container");
-    this.careerPaths.forEach(careerPath => {
-      const item = document.createElement("li");
-      item.innerHTML = careerPath.name;
-      item.classList.add("list-group-item");
-      list.appendChild(item);
-    });
+    list.innerHTML = this.careerPaths
+      .map(path => `<li class="list-group-item">${path.name}</li>`)
+      .join(" ");
   }
 }
