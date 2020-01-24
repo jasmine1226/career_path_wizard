29940d2f547b0912bd26b5146632d3b18921646b
jasmine1226 <jasmine1226@gmail.com>
2020-01-24 00:12:16 -0800

    moved course form to courses.js

--- frontend/src/components/careerPaths.js
+++ frontend/src/components/careerPaths.js
@@ -8,14 +8,10 @@ class CareerPaths {
 
   initBindingsAndEventListeners() {
     this.careerPathContainer = document.getElementById("career-path-container");
-    this.courseForm = document.getElementById("new-course-form");
-    this.courseTitle = document.getElementById("course-title");
-    this.courseUrl = document.getElementById("course-url");
     this.careerPathContainer.addEventListener("click", event => {
       this.careerPathId = event.target.dataset.id;
       this.renderCourses(this.careerPathId);
     });
-    this.courseForm.addEventListener("submit", this.createCourse.bind(this));
   }
 
   fetchAndLoadCareerPaths() {
@@ -45,23 +41,5 @@ class CareerPaths {
     this.careerPathContainer.innerHTML = this.careerPaths
       .map(careerPath => careerPath.renderLi())
       .join(" ");
-  }
-
-  createCourse(e) {
-    e.preventDefault();
-    const course = {
-      title: this.courseTitle.value,
-      url: this.courseUrl.value,
-      career_path_id: this.careerPathId
-    };
-    const careerPath = this.careerPaths.find(
-      careerPath => careerPath.id === course.career_path_id
-    );
-    this.adapter
-      .createCourse(course)
-      .then(c => {
-        careerPath.courses.push(new Course(c.data));
-      })
-      .then(() => careerPath.renderCourses());
   }
 }
