5f7c1d92299753beb41dd61fbf352918c0cf6262
jasmine1226 <jasmine1226@gmail.com>
2020-01-24 00:20:44 -0800

    separated courseAdapter

--- frontend/src/adapters/careerPathAdapter.js
+++ frontend/src/adapters/careerPathAdapter.js
@@ -1,29 +1,13 @@
 class CareerPathAdapter {
   constructor() {
-    this.baseURL = "http://localhost:3000/api/v1";
+    this.baseURL = "http://localhost:3000/api/v1/career_paths/";
   }
 
   getCareerPaths() {
-    return fetch(this.baseURL + "/career_paths").then(res => res.json());
+    return fetch(this.baseURL).then(res => res.json());
   }
 
   getCareerPath(id) {
-    return fetch(this.baseURL + "/career_paths/" + id).then(res => res.json());
-  }
-
-  getCourses(id) {
-    return fetch(this.baseURL + "/career_paths/" + id + "/courses").then(res =>
-      res.json()
-    );
-  }
-
-  createCourse(course) {
-    return fetch(this.baseURL + "/courses", {
-      method: "POST",
-      headers: {
-        "content-type": "application/json"
-      },
-      body: JSON.stringify(course)
-    }).then(res => res.json());
+    return fetch(this.baseURL + id).then(res => res.json());
   }
 }
